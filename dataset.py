68e34db6db31237fe3a38606882fc14d64e1dccb
zjkjzj <wy163zhuj@163.com>
2023-09-29 11:30:47 +0800

    feat(wr2): model / train

---
+++ dataset.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+
+"""
+@date: 2023/9/27 下午5:04
+@file: dataset.py
+@author: zj
+@description: 
+"""
+
+import os
+import cv2
+
+import numpy as np
+from tqdm import tqdm
+from pathlib import Path
+
+import torch
+from torch.utils.data import Dataset
+
+
+def load_data(data_root, pattern='*.json'):
+    assert os.path.isdir(data_root)
+
+    data_list = list()
+
+    p = Path(data_root)
+    for path in tqdm(p.rglob(pattern)):
+        data_list.append(str(path).strip())
+
+    return data_list
+
+
+def build_data(img_path):
+    img = cv2.imread(img_path)
+    img_h, img_w = img.shape[:2]
+    img = cv2.resize(img, (480, 480))
+
+    # HWC -> CHW
+    data = torch.from_numpy(img).permute((2, 0, 1)).float()
+    data /= 255.0
+
+    return data, (img_h, img_w)
+
+
+def build_target(img_path, img_h, img_w):
+    # img_name: 025-95_113-154&383_386&473-386&473_177&454_154&383_363&402-0_0_22_27_27_33_16-37-15.jpg
+    img_name = os.path.basename(img_path)
+
+    # ['025', '95_113', '154&383_386&473', '386&473_177&454_154&383_363&402', '0_0_22_27_27_33_16', '37', '15']
+    all_infos = img_name.rsplit('/', 1)[-1].rsplit('.', 1)[0].split('-')
+    # print(f"all_infos: {all_infos}")
+
+    # left-top / right-bottom
+    # [[x1, y1], [x2, y2]]
+    box_xyxy = [[int(eel) for eel in el.split('&')] for el in all_infos[2].split('_')]
+    x1, y1 = box_xyxy[0]
+    x2, y2 = box_xyxy[1]
+
+    x_c = (x1 + x2) / 2.
+    y_c = (y1 + y2) / 2.
+    box_w = x2 - x1
+    box_h = y2 - y1
+    label = [x_c / img_w, y_c / img_h, box_w / img_w, box_h / img_h]
+
+    label.extend([int(x) for x in all_infos[4].split("_")])
+
+    return torch.from_numpy(np.array(label, dtype=float))
+
+
+class CCPD(Dataset):
+
+    def __init__(self, data_root, only_lp=False):
+        self.data_root = data_root
+        self.only_lp = only_lp
+
+        print(f"Get Data: {data_root}")
+        self.data_list = load_data(data_root, pattern="*.jpg")
+        print(f"Dataset len: {len(self.data_list)}")
+
+    def __getitem__(self, index):
+        img_path = self.data_list[index]
+
+        image, (img_h, img_w) = build_data(img_path)
+        label = build_target(img_path, img_h, img_w)
+
+        return image, label
+
+    def __len__(self):
+        return len(self.data_list)
---
+++ train_wr2.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+
+"""
+@date: 2023/9/27 下午5:00
+@file: train.py
+@author: zj
+@description: 
+"""
+
+import argparse
+import math
+import os.path
+
+from tqdm import tqdm
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+
+from wr2 import wR2
+from loss import wR2Loss
+from dataset import CCPD
+from evaluator import CCPDEvaluator
+
+
+def parse_opt():
+    parser = argparse.ArgumentParser(description='Training wR2')
+    parser.add_argument('train_root', metavar='DIR', type=str, help='path to train dataset')
+    parser.add_argument('val_root', metavar='DIR', type=str, help='path to val dataset')
+    parser.add_argument('output', metavar='OUTPUT', type=str, help='path to output')
+
+    args = parser.parse_args()
+    print(f"args: {args}")
+    return args
+
+
+def train(train_root, val_root, output):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = wR2(num_classes=4).to(device)
+    criterion = wR2Loss().to(device)
+
+    learn_rate = 0.001
+    optimizer = optim.SGD(model.parameters(), lr=learn_rate, momentum=0.9, weight_decay=5e-4)
+    scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[40, 70, 90])
+
+    train_dataset = CCPD(train_root)
+    train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=True, num_workers=4, drop_last=False,
+                                  pin_memory=True)
+    val_dataset = CCPD(val_root)
+    val_dataloader = DataLoader(val_dataset, batch_size=32, shuffle=False, num_workers=4, drop_last=False,
+                                pin_memory=True)
+
+    ccpd_evaluator = CCPDEvaluator(only_det=True)
+
+    print("Start training")
+    epochs = 100
+    warmup_epoch = 5
+    for epoch in range(1, epochs + 1):
+        # epoch: start from 1
+        model.train()
+
+        pbar = tqdm(train_dataloader)
+        for idx, (images, targets) in enumerate(pbar):
+            images = images.to(device)
+            targets = targets.to(device)
+
+            outputs = model(images)
+
+            loss = criterion(outputs, targets)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            lr = optimizer.param_groups[0]["lr"]
+            info = "Epoch:%d LR:%f Loss:%f" % (epoch, lr, loss)
+            pbar.set_description(info)
+
+        if epoch % 5 == 0 and epoch > 0:
+            model.eval()
+            save_path = os.path.join(output, f"wR2-e{epoch}.pth")
+            print(f"Save to {save_path}")
+            torch.save(model.state_dict(), save_path)
+
+            pbar = tqdm(val_dataloader)
+            for idx, (images, targets) in enumerate(pbar):
+                images = images.to(device)
+                targets = targets.to(device)
+
+                with torch.no_grad():
+                    outputs = model(images)
+
+                ap, _ = ccpd_evaluator.update(outputs, targets)
+                info = "Idx:%d AP:%f" % (idx, ap)
+                pbar.set_description(info)
+            ap, _ = ccpd_evaluator.result()
+            print(f"AP: {ap}")
+        scheduler.step()
+
+
+def main():
+    args = parse_opt()
+
+    output = args.output
+    if not os.path.exists(output):
+        os.makedirs(output)
+
+    train(args.train_root, args.val_root, output)
+
+
+if __name__ == '__main__':
+    main()
